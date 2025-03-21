0e676a648935b8c2c9b39f0d57288ed50dfbe606
RaulBecerraB <ingenierobecerrabarcelo@gmail.com>
2025-03-21 14:24:51 -0600

    Add team information and project title 'Generador de tabla de triplos'

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
+"""
+Lenguajes y Autómatas II 
+"Generador de tabla de triplos" 
+Becerra Barceló José Raúl. 
+Diaz Sanches Edwin Ulises. 
+Ferrer Chacón Diego Felipe. 
+7SA 
+E21080774 
+E21081367 
+E21080738
+"""
+
 from cli import main
 
 if __name__ == "__main__":
